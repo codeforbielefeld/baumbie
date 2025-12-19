fa766163a3acf032cfb8841055da71257b9aa5d7
Marcel Otto <marcelotto@gmx.de>
2025-12-19 13:17:06 +0100

    Init Xylem project
    $ mix new xylem
    
    with Elixir v1.18.4

---
+++ xylem/lib/xylem.ex
@@ -0,0 +1,18 @@
+defmodule Xylem do
+  @moduledoc """
+  Documentation for `Xylem`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Xylem.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ xylem/test/xylem_test.exs
@@ -0,0 +1,8 @@
+defmodule XylemTest do
+  use ExUnit.Case
+  doctest Xylem
+
+  test "greets the world" do
+    assert Xylem.hello() == :world
+  end
+end
